2021d3111a590883036e367129e971f6bef8f840
小灰灰 <127174664+iShan61322@users.noreply.github.com>
2023-05-09 16:39:26 +0800

    Update 字符串.c

--- 字符串.c
+++ 字符串.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-#include<string.h>
+#include<string.h>//运用strlen()所要引用的头文件
 
 int main()
 {
@@ -7,9 +7,9 @@ int main()
 	char arr2[]={'a','b','c','d'};
 	char arr3[]={'a','b','c','\0'};
 	
-	printf("%d\n",strlen(arr1));
-	printf("%d\n",strlen(arr2));
-	printf("%d\n",strlen(arr3));
+	printf("%d\n",strlen(arr1));//输出结果为3
+	printf("%d\n",strlen(arr2));//输出结果为随机数，因为没有结束字符\0
+	printf("%d\n",strlen(arr3));//输出结果为3
 	
 	return 0;
 }
