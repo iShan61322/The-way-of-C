e6b8d6ca3fcf5aa318dc2d69d15250892d5b918a
ToAtob <127174664+iShan61322@users.noreply.github.com>
2023-05-15 18:43:52 +0800

    Add files via upload

---
+++ 循环语句while.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+int main()
+{
+	int i = 0;
+	while (i <= 613)
+	{
+		if (i == 613)
+		{
+			printf("°³°®ÓêÉº£¡");
+				break;
+		}
+		printf("Å£ÓêÉº´óÍõ\n");
+		i++;
+	}
+	return 0;
+}
