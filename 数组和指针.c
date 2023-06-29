1b11d62ebc7c9d3182c100e551b62118eba1f6b5
ToAtob <127174664+iShan61322@users.noreply.github.com>
2023-06-29 11:30:39 +0800

    Add files via upload

---
+++ 数组和指针.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+int main()
+{
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	int* p = arr;//数组名为首元素地址
+	//arr[2] <==> *(arr+2) <==> *(p+2) <==> *(2+p) <==> *(2+arr) <==> 2[arr]
+
+	printf("%d\n", arr[2]);
+	printf("%d\n", 2[arr]);
+	printf("%d\n", 2[p]);
+	printf("%d\n", p[2]);
+	printf("%d\n", *(p+2));
+
+	return 0;
+}
